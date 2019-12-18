1b442236dd067c4ee87672e40774d631fc044c2d
Thabet <thabet@souktel.org>
2019-12-19 00:20:57 +0200

    moving helper functions away from pipeline

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -1,3 +1,69 @@
+def loadEnvvars(String path) {
+    try {
+        node {
+            checkout scm
+            Map envvars = readProperties(interpolate: true, file: path) ?: null
+            if (!envvars) { throw new Exception("unable to read properties file " + path) }
+            keys= envvars.keySet()
+            for(key in keys) {
+                value = envvars["${key}"]
+                env."${key}" = "${value}"
+            }
+
+            env.ENV = getENV()
+            env.ENV_NAME = getEnvName()
+            env.DOMAIN = getDomain()
+            env.BUILD_LABEL = getBuildLabel()
+            env.AGENT_LABEL = getAgentLabel()
+
+            env.IMAGE_TAG = "${BUILD_ID}"
+            env.CONTAINER_NAME = "${DOCKER_IMAGE}_${IMAGE_TAG}"
+            env.LOCAL_IMAGE_NAME = "${DOCKER_IMAGE}:${IMAGE_TAG}"
+            env.REGISTRY_IMAGE_NAME = "${DOCKER_REGISTRY}/${LOCAL_IMAGE_NAME}"
+            
+            env.DOCKER_REGISTRY_URL = "https://${DOCKER_REGISTRY}"
+            env.ECR_REGISTRY_CREDENTIAL = "ecr:${AWS_REGION}:${AWS_CREDENTIALS}"
+
+            return envvars
+        }
+    }
+    catch(err) {
+        echo "ERROR: caught in loadEnvvars ${err}"
+    }
+}
+
+def getENV() {
+    if (env."ENV_${BRANCH_NAME.toUpperCase()}") {
+        return env."ENV_${BRANCH_NAME.toUpperCase()}"
+    }
+    return env.ENV_DEVELOP
+}
+
+def getEnvName() {
+    if (env."NAME_${env.ENV}") {
+        return env."NAME_${env.ENV}"
+    }
+    return env.NAME_DEV
+}
+
+def getBuildLabel() {
+    if (env."AGENT_BUILD") {
+        return env."AGENT_BUILD"
+    }
+    return env.AGENT_DEV
+}
+
+def getAgentLabel() {
+    if (env."AGENT_${env.ENV}") {
+        return env."AGENT_${env.ENV}"
+    }
+    return env.AGENT_DEV
+}
+
+def getDomain() {
+    return 'https://' + env."DOMAIN_${env.ENV.toUpperCase()}"
+}
+
 def curlstatus(String url) {
     assert url != null
     int status = sh(label: 'Domain Check',
